51a6eabf24f9cc6a3c21d701c4900f73f009fb12
mathias77515 <mathias.p.regnier@gmail.com>
2025-06-05 12:46:15 +0200

    add files

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+import pybind11
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path"""
+    def __str__(self):
+        return pybind11.get_include()
+
+ext_modules = [
+    Extension(
+        'UF23Field',                  # name of the module
+        ['LoopcalcUF23Field.cxx', 'UF23Field.cc'],  # your source files
+        include_dirs=[
+            get_pybind_include(),
+            # add other include dirs, e.g. current directory, or where UF23Field.h lives
+            '.',
+        ],
+        language='c++',
+        extra_compile_args=['-std=c++17'],  # match your C++ standard
+    ),
+]
+
+setup(
+    name='model',
+    version='0.0.1',
+    author='Your Name',
+    author_email='you@example.com',
+    description='Pybind11 wrapper for UF23Field',
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext},
+    zip_safe=False,
+)
+
+
+#python setup.py build_ext --inplace
